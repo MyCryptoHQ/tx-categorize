dfff2cef5a1b65ed024b26cd45bd69e167a743a8
blurpesec <hahn.michael.f@gmail.com>
2021-05-04 15:24:32 -0700

    re-added generate-test-tempalte

--- cli/cmd/generateTestTemplate.go
+++ cli/cmd/generateTestTemplate.go
@@ -1,6 +1,15 @@
 package cmd
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+
+	"github.com/mycryptohq/tx-categorize/cmd/build"
+	"github.com/mycryptohq/tx-categorize/cmd/categorize"
+	"github.com/mycryptohq/tx-categorize/common/etherclient"
+	"github.com/mycryptohq/tx-categorize/types"
 	"github.com/spf13/cobra"
 )
 
@@ -9,27 +18,36 @@ var generateTestTemplateCmd = &cobra.Command{
 	Use:   "generate-test-template",
 	Short: "Generates a test template based on an input tx hash",
 	Run: func(cmd *cobra.Command, args []string) {
-		// rpcUrl, _ := cmd.Flags().GetString("rpc")
-		// if rpcUrl == "" {
-		// 	log.Fatal(fmt.Errorf("rpcUrl is required but not found"))
-		// }
-		// schemaId, _ := cmd.Flags().GetString("schemaId")
-		// if schemaId == "" {
-		// 	log.Fatal(fmt.Errorf("schemaId is required but not found"))
-		// }
-		// txHash, _ := cmd.Flags().GetString("txHash")
-		// if txHash == "" {
-		// 	log.Fatal(fmt.Errorf("txHash is required but not found"))
-		// }
-		// client := etherclient.MakeETHClient(rpcUrl)
-		// txHashes := []string{txHash}
-		// txConstructions := build.FetchTxReceipts(txHashes, *client)
-		// formattedNormalTxs := categorize.FormatNormalTxsToStandard(txConstructions)
-		// schemas, _ := categorize.FetchAndWalkSchema("./../schema/")
-		// file, _ := json.MarshalIndent(rec, "", "  ")
-		// fileLoc := fmt.Sprintf("../tests/test_txs/%s.json", schemaId)
-		// _ = ioutil.WriteFile(fileLoc, file, 0644)
-		//fmt.Println("A empty schema template file has been generated at", fileLoc)
+		rpcUrl, _ := cmd.Flags().GetString("rpc")
+		if rpcUrl == "" {
+			log.Fatal(fmt.Errorf("rpcUrl is required but not found"))
+		}
+		schemaId, _ := cmd.Flags().GetString("schemaId")
+		if schemaId == "" {
+			log.Fatal(fmt.Errorf("schemaId is required but not found"))
+		}
+		txHash, _ := cmd.Flags().GetString("txHash")
+		if txHash == "" {
+			log.Fatal(fmt.Errorf("txHash is required but not found"))
+		}
+		client := etherclient.MakeETHClient(rpcUrl)
+		txHashes := []string{txHash}
+		txConstructions := build.FetchTxReceipts(txHashes, *client)
+		formattedNormalTxs := categorize.FormatNormalTxsToStandard(txConstructions)
+		schemas, _ := categorize.FetchAndWalkSchema("./../schema/")
+		var out []types.StandardTx
+		for _, tx := range formattedNormalTxs {
+			finalTx, err := categorize.DetermineTxType(tx, schemas)
+			if err != nil {
+				fmt.Println("[generate-test-template]: error - ", err)
+			} else {
+				out = append(out, finalTx)
+			}
+		}
+		file, _ := json.MarshalIndent(out, "", "  ")
+		fileLoc := fmt.Sprintf("../tests/test_txs/%s.json", schemaId)
+		_ = ioutil.WriteFile(fileLoc, file, 0644)
+		fmt.Println("A empty schema template file has been generated at", fileLoc)
 	},
 }
 
